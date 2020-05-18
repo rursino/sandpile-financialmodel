b6bbea78a26f518453f212e768a75a738e6cb3ef
rursino <rossursino@hotmail.com>
2020-05-19 09:30:43 +1000

    Scale invariance complete and proven

--- python/scripts/scale_invariance.py
+++ python/scripts/scale_invariance.py
@@ -3,6 +3,7 @@ Scale invariance means that the structure of the observables and grid is
 (roughly) unchanged to changes in the dimensions of the grid.
 """
 
+""" IMPORTS """
 import numpy as np
 import sys
 import matplotlib.pyplot as plt
@@ -15,28 +16,31 @@ reload(sandpile)
 
 import main as main_script
 
+
 """ INPUTS """
-dimensions = [5, 10, 100, 1000, 10000]
+dimensions = [2, 5, 10, 50, 100]
 
 
 """ FUNCTIONS """
-def fn():
-    raise NotImplementedError()
-
 def main():
+    # Directory to save output plots.
+    dir = "./../output/scale_invariance/"
+
+    # Create a histogram of an observable for different grid dimensions.
     for lw in dimensions:
         sp = sandpile.SandPile(lw, lw)
 
         for i in range(10000):
             main_script.execute_avalanches(sp)
 
-        fname = f"./../output/scale_invariance.pik"
+        fname = f"{dir}stats.pik"
         sp.save_avalanche_stats(fname)
 
         ob = sandpile.Observables(fname)
-        subplot = ob.histogram(ob.radius, density=1)
+        ob.histogram(ob.topples, density=0)
+        plt.savefig(f"{dir}histogram_{lw}.png")
 
 
 """ EXECUTION """
-if __name__ = "__main__":
+if __name__ == "__main__":
     main()
