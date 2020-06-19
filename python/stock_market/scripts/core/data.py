46e411f13cfb7a6b7010dd3582bb1ef087d080b7
rursino <rossursino@hotmail.com>
2020-06-20 00:42:38 +1000

    Crash detection works

--- python/stock_market/scripts/core/data.py
+++ python/stock_market/scripts/core/data.py
@@ -17,21 +17,41 @@ x = df.index
 y1 = df.Open
 y2 = df.Volume
 
-ipeaks = signal.find_peaks(y1)[0]
-y1[ipeaks[45]]
+imin = signal.argrelmin(y1.values, order=6)[0]
+imax = signal.argrelmax(y1.values, order=6)[0]
+
 fig = plt.figure(figsize=(20,10))
 ax = fig.add_subplot(111)
 ax.plot(y1)
 ax.set_ylabel('Day Open Price')
-plt.scatter(ipeaks, y1[ipeaks], color='r')
+plt.scatter(imin, y1[imin], color='r')
+plt.scatter(imax, y1[imax], color='b')
 
-def detect_a_crash(x):
+def crash_detection(x):
     """ Detects a crash in an index fund timeseries, which is defined by a drop
     in the stock price by 20% from a local peak.
     """
 
-    ipeaks = signal.find_peaks(x)[0]
-    
+    order = int(len(x)/42)
+    imin = signal.argrelmin(x.values, order = order)[0]
+    imax = signal.argrelmax(x.values, order = order)[0]
+
+    crash = []
+    for maxi, max_index in enumerate(imax):
+        try:
+            min_index = imin[(max_index < imin) & (imin < imax[maxi + 1])][-1]
+        except IndexError:
+            pass
+
+        sub_x = x[max_index : min_index + 1]
+
+        threshold = 0.8 * x[max_index]
+        if np.any(sub_x < threshold):
+            crash.append(sub_x)
+
     return crash
 
-detect_a_crash(y1)
+crash_detection(y1)
+
+imin
+imax
