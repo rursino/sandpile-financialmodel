d40fa557e103f4ed5a7e3b4e9e8b5f3a867ea75b
rursino <rossursino@hotmail.com>
2020-06-19 18:24:32 +1000

    crash detection

--- python/stock_market/scripts/core/data.py
+++ python/stock_market/scripts/core/data.py
@@ -6,18 +6,19 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from scipy import signal
+import numpy as np
 
 """ S&P 500 """
 df = pd.read_csv("./../../data/sp500daily.csv",
                 index_col = "Date"
                 )
-df
+
 x = df.index
 y1 = df.Open
 y2 = df.Volume
 
 ipeaks = signal.find_peaks(y1)[0]
-
+y1[ipeaks[45]]
 fig = plt.figure(figsize=(20,10))
 ax = fig.add_subplot(111)
 ax.plot(y1)
@@ -29,6 +30,8 @@ def detect_a_crash(x):
     in the stock price by 20% from a local peak.
     """
 
-    ipeaks = signal.find_peaks(x)
+    ipeaks = signal.find_peaks(x)[0]
+    
+    return crash
 
-    for peak in ipeak
+detect_a_crash(y1)
