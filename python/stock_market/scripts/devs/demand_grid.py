c7aa9dc0e5674f5ea59b4cd89c7528c14f85169c
rursino <rossursino@hotmail.com>
2020-06-26 16:43:18 +1000

    Entire project complete

--- python/stock_market/scripts/devs/demand_grid.py
+++ python/stock_market/scripts/devs/demand_grid.py
@@ -15,7 +15,12 @@ y = 0.8 * stats.norm.cdf(x=x,
                         loc=0.5*n,
                         scale=0.2*n
                         )
+plt.figure(figsize=(20,10))
 plt.plot(x, y); plt.plot(x, 0.8-y)
+plt.title("Probability of three events vs. Units held by an investor",
+            fontsize=24)
+plt.xlabel("Units held by an investor", fontsize=16)
+plt.ylabel("Probability", fontsize=16)
 
 units = 50
 p = 1 - stats.powerlaw.cdf(x=np.arange(units*0.25),
@@ -25,6 +30,12 @@ p = 1 - stats.powerlaw.cdf(x=np.arange(units*0.25),
                         )
 p = np.concatenate([p, 0.0 + np.zeros(int(units*0.75))])
 p /= sum(p)
+
+plt.figure(figsize=(20,10))
 plt.plot(p); plt.xlim([0, units])
+plt.title("Probability of units to transact",
+            fontsize=24)
+plt.xlabel("Units held by an investor", fontsize=16)
+plt.ylabel("Probability", fontsize=16)
 
 np.random.choice(units, p=p)
